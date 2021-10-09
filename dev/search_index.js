e032938573f4000b776850f5c874faa87df31915
Documenter.jl <documenter@juliadocs.github.io>
2021-10-09 22:16:59 +0000

    build based on c24b947

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ISA","category":"page"},{"location":"#ISA","page":"Home","title":"ISA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ISA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ISA]","category":"page"}]
+}
