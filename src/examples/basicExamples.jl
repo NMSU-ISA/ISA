3bb3aec4470d0538a1e38b1dfcebb848f5ed7a49
Mamta <mamta16@nmsu.edu>
2021-10-21 16:48:06 -0600

    added examples

--- src/examples/basicExamples.jl
+++ src/examples/basicExamples.jl
@@ -9,29 +9,27 @@ using ISA #References: Sandoval, Steven, and Phillip L. De Leon. "The Instantane
 a₀(t) = exp(-t^2)
 ω₀(t) = 100
 φ₀ = 0
-𝐶₀ = Tuple([a₀,ω₀,φ₀])
-
+𝐶₀ = AMFMtriplet(a₀,ω₀,φ₀)
 #DEFINE 0th COMPONENT
 ψ₀ = AMFMcomp(𝐶₀)
-
 #DEFINE 1st CANONICAL TRIPLET
 a₁(t) = 1
 ω₁(t) = 10*t
 φ₁ = π
-𝐶₁ = Tuple([a₁,ω₁,φ₁])
-
+𝐶₁ = AMFMtriplet(a₁,ω₁,φ₁)
+#DEFINE 1st COMPONENT
+ψ₁ = AMFMcomp(𝐶₁)
 #DEFINE 2nd CANONICAL TRIPLET
 a₂(t) = 0.8*cos(2t)
 ω₂(t) = 10 + 7.5*sin(t)
 φ₂ = π
-𝐶₂ = Tuple([a₂,ω₂,φ₂])
-
+𝐶₂ = AMFMtriplet(a₂,ω₂,φ₂)
+#DEFINE 2nd COMPONENT
+ψ₂ = AMFMcomp(𝐶₂)
 #DEFINE THE COMPONENT SET
-𝑆 = [𝐶₀,𝐶₁,𝐶₂]
-
+𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 #DEFINE THE AMFM MODEL
 z = AMFMmodel(𝑆)
-
 #DEFINE A TIME INDEX
 t = Array(0.0:0.005:2.0)
 
---
+++ src/examples/basicExamples1.jl
@@ -0,0 +1,3 @@
+using ISA
+C = AMFMtriplet(t->exp(-t^2),ω->100,0.0)
+ψ = AMFMcomp(C)
