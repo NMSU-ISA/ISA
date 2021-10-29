74951ce368385502aac96ca478aa9de914ef5e27
Mamta <mamta16@nmsu.edu>
2021-10-28 19:23:26 -0600

    updated test cases

--- test/analyticalCompTests.jl
+++ test/analyticalCompTests.jl
@@ -5,23 +5,36 @@ S = compSet([C,C])
 ψ = AMFMcomp(C)
 
 a₀(t) = exp(-t^2)
-ω₀(t) = 2.0
+ω₀(t) = 2.0*t
 φ₀ = 0.0
 C₀ = AMFMtriplet(a₀,ω₀,φ₀)
-ψ₀= AMFMcomp(a₀,ω₀,φ₀)
-t₀ = 2.0
+ψ = AMFMcomp(C₀)
+#ψ₀= AMFMcomp(a₀,ω₀,φ₀)
+t₀ = 1.0
 t=collect(1:5)
+ψ(t₀)
 ψ(t)
 ψ([2.0])
 ψ(2.0)
 ψ(1:10)
 
+a₁(t) = 1.0
+ω₁(t) = 10*t
+φ₁ = 0.1
+C₁ = AMFMtriplet(a₁,ω₁,φ₁)
+
 
 <<<<<<< Updated upstream
 
+C₀ = AMFMtriplet(t->cos.(t),ω->100,0.1)
+C₁ = AMFMtriplet(t->2*t,ω->10,1.0)
+S = compSet([C₀,C₁])
+
+
+
 
 z = AMFMmodel(S)
-z(1.0)
+z(3.0)
 
 isaPlot3d_PlotsGR(z,collect(-1.0:0.001:1.0))
 =======
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,38 @@ using ISA
 using Test
 using LinearAlgebra
 @testset "ISA.jl" begin
+
     C = AMFMtriplet(t->exp(-t^2),ω->100,0.0)
     ψ = AMFMcomp(C) #method 1
-    #ψ = AMFMcomp(t->exp(-t^2),ω->100,0.0) #method 2
 
     @test norm(ψ(2.0).-[0.00892315352647076 - 0.015994997938278837im]) ≈0.0
     @test norm(ψ(collect(1:3)).-[0.31722938484878416 - 0.1862815090798727im,0.00892315352647076 - 0.015994997938278837im,-2.7269394561533185e-6 - 0.0001233796723367148im]) ≈0.0
+
+    a(t) = exp(-t^2)
+    ω(t) = 2.0*t
+    φ = 0.0
+    ψ = AMFMcomp(a,ω,φ) #method 2
+    @test norm(ψ(1.0) .- [0.19876611034641298 + 0.3095598756531122im]) ≈0.0
+
+    C₀ = AMFMtriplet(t->sin.(t),ω->10,0.1)
+    C₁ = AMFMtriplet(t->2,ω->100,0.0)
+    S = compSet([C₀,C₁])
+    z = AMFMmodel(S) #method 1
+
+    @test norm(z(1.0).-[1.0678122692987118 - 1.5387100967178977im]) ≈0.0
+
+    C₀ = AMFMtriplet(t->cos.(t),ω->100,0.1)
+    ψ₀ = AMFMcomp(C₀)
+    C₁ = AMFMtriplet(t->2,ω->10,0.0)
+    ψ₁ = AMFMcomp(C₁)
+    ψ = [ψ₀,ψ₁]
+    z = AMFMmodel(ψ) #method 2
+    @test norm(z(2.0).-[0.578153926388216 + 2.1672544356642636im]) ≈0.0
+
+    C₀ = AMFMtriplet(t->cos.(t),ω->100,0.1)
+    C₁ = AMFMtriplet(t->2*t,ω->10,1.0)
+    C = [C₀,C₁]
+    z = AMFMmodel(C) #method 3
+    @test norm(z(3.0).-[5.411410145804563 - 1.4372358186796916im]) ≈0.0
+
 end
