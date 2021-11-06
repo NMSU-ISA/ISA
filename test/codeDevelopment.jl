5945bb4f1fa01a7fbc7625895e3b132bdb88aa45
Mamta <mamta16@nmsu.edu>
2021-11-06 15:43:10 -0600

    done test cases

--- test/codeDevelopment.jl
+++ test/codeDevelopment.jl
@@ -14,3 +14,16 @@ z=AMFMmodel(S1)
 z2(3.0)
 z1(2.0)
 z1(collect(1:2))
+
+
+
+C₀ = AMFMtriplet(t->cos.(t),ω->100,0.1)
+C₁ = AMFMtriplet(t->2*t,ω->10,1.0)
+ψ₀ = AMFMcomp(C₀)
+ψ₁ = AMFMcomp(C₁)
+S=compSet([ψ₀,ψ₁]) # Method 2 for calling compSet
+z = AMFMmodel(S)
+z(2.0)
+z([1.0,5.0])
+z(collect(1:3))
+z(1:0.1:2)
