7c4257ba793cc12c8fed9acc8d0b55df9e1e1e42
Mamta <mamta16@nmsu.edu>
2021-11-06 19:56:19 -0700

    updated examples

--- src/examples/basicExamples2.jl
+++ src/examples/basicExamples2.jl
@@ -1,15 +1,18 @@
-# Evaluating the AM-FM model at a time instant of type Float64.
+# Evaluating the AM-FM model at a time instant
+# of type Float64.
 julia> t₀ = 2.0; z(t₀)
 -0.14301691708182013 - 0.40883650170394287im
 
-# Evaluating the AM-FM model over a step range of time instants.
+# Evaluating the AM-FM model over a step range
+# of time instants.
 julia> t=1.0:0.1:1.2; z(t)
 3-element Vector{ComplexF64}:
  -0.22297667728629453 - 0.02816723727420639im
    0.1525504825286645 - 0.17212705030880993im
    0.3545611578416981 + 0.07197075870884703im
 
-# Evaluating the AM-FM model at time instants of type Vector{Float64}.
+# Evaluating the AM-FM model at time instants
+# of type Vector{Float64}.
 julia> t = collect(1.0:3.0); z(t)
 3-element Vector{ComplexF64}:
  -0.22297667728629453 - 0.02816723727420639im
