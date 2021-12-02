9531a0e1af7ac9dd1cbb36138f0abc5913f7f021
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-02 21:26:42 +0000

    build based on accc75f

--- dev/assets/src/IS_ex2.jl
+++ dev/assets/src/IS_ex2.jl
@@ -1,7 +1,7 @@
 using ISA, Plots
-𝐶₀ = AMFMtriplet(t->exp(-t^2),t->200.0,0.0)
+𝐶₀ = AMFMtriplet(t->exp(-t^2/5),t->200.0,0.0)
 𝐶₁ = AMFMtriplet(t->1.0,t->100*t,0.1)
-𝐶₂ = AMFMtriplet(t->0.8*cos(2t),t->100 + 70.5*sin(5t),π)
+𝐶₂ = AMFMtriplet(t->0.8*cos(11t),t->100 + 70.5*sin(5t),π)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 plot(𝑆; timeaxis=0.0:0.005:3.0, camera=(0,90), zlabel="", zticks=:false,  left_margin=15Plots.mm, margin=5Plots.mm, yrotation = 90)
 
