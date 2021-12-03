afbc84909dadeb95260264e6d145b55115319228
Steven Sandoval <ssandova@gmail.com>
2021-12-03 09:09:49 -0700

    docs

--- src/models/numericalModels.jl
+++ src/models/numericalModels.jl
@@ -46,7 +46,7 @@ using ISA
 ψ₁ = AMFMcomp(t->1.0,t->10*t,0.1)
 ψ₂ = AMFMcomp(t->0.8*cos(2t),t->10 + 7.5*sin(t),π)
 fs = 16_000
-t = collect{0:1/fs:1}
+t = collect(0:1/fs:1)
 𝛹₀ = numComp( ψ₀(t), fs )
 𝛹₁ = numComp( ψ₁(t), fs )
 𝛹₂ = numComp( ψ₂(t), fs )
