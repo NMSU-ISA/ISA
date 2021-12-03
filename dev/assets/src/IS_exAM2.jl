1de1f9ee885c7d69e4b669bca1d59b660c5bd2d9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-03 23:21:32 +0000

    build based on 25cf5e2

--- dev/assets/src/IS_exAM2.jl
+++ dev/assets/src/IS_exAM2.jl
@@ -1,5 +1,4 @@
 using ISA, Plots
-𝐶₀ = AMFMtriplet(t->sin(11t),t->250,0.0)
-ψ₀ = AMFMcomp(𝐶₀)
-plot(ψ₀; timeaxis=-1.0:0.001:1.0)
+𝐶₀ = AMFMtriplet(t->1,t->100,0.0)
+plot(𝐶₀; timeaxis=-1.0:0.001:1.0)
 png("IS_exAM2.png")
