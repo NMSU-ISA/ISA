83f8d40a7892a4c0a595ec9988d239854634c03e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-04 17:25:12 +0000

    build based on e7db192

---
+++ dev/assets/src/IS_exFM.jl
@@ -0,0 +1,33 @@
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->t^2,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+png("IS_exFM1.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->t^2,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90)
+png("IS_exFM2.png")
+
+# ----------------
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->25+20*sin(t),0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+png("IS_exFM3.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->25+20*sin(t),0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90)
+png("IS_exFM4.png")
