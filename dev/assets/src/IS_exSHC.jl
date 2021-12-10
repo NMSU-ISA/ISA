522f24ae46f1207f95e69a2c0048a4428f1d53be
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-10 17:50:42 +0000

    build based on 69b7877

--- dev/assets/src/IS_exSHC.jl
+++ dev/assets/src/IS_exSHC.jl
@@ -21,3 +21,49 @@ plot!( camera=(0,90),
        left_margin=15Plots.mm, margin=5Plots.mm,
        yrotation = 90, ymirror=true)
 png(path*"IS_exSHC3.png")
+
+#------------------------------------
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->10π,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(ψ₀; timeaxis=-1.0:0.001:1.0)
+png(path*"IS_exSHC4.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->10π,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=-1.0:0.001:1.0)
+png(path*"IS_exSHC5.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->1,t->10π,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=-1.0:0.001:1.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90, ymirror=true)
+png(path*"IS_exSHC6.png")
+
+#-----------------------------------
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->10,t->exp(π),1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(ψ₀; timeaxis=-1.0:0.001:1.0)
+png(path*"IS_exSHC7.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->10,t->exp(π),1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=-1.0:0.001:1.0)
+png(path*"IS_exSHC8.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->10,t->exp(π),1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=-1.0:0.001:1.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90, ymirror=true)
+png(path*"IS_exSHC9.png")
