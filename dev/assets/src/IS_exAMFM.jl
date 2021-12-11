fa846c5daff06a07acf6d53646b50522a04d71f6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-11 05:39:06 +0000

    build based on c736898

---
+++ dev/assets/src/IS_exAMFM.jl
@@ -0,0 +1,71 @@
+path = "docs/src/assets/"
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->t^2,t->2cos(10t),0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(ψ₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM1.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->t^2,t->2cos(10t),0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM2.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->t^2,t->2cos(10t),0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90, ymirror=true)
+png(path*"IS_exAMFM3.png")
+
+#--------------------------
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(t),t->cos(5t)+50,1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(ψ₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM4.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(t),t->cos(5t)+50,1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM5.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(t),t->cos(5t)+50,1.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90, ymirror=true)
+png(path*"IS_exAMFM6.png")
+
+#----------------------------
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(0.1t),t->t^2+t+10,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(ψ₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM7.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(0.1t),t->t^2+t+10,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+png(path*"IS_exAMFM8.png")
+
+using ISA, Plots
+𝐶₀ = AMFMtriplet(t->exp(0.1t),t->t^2+t+10,0.0)
+ψ₀ = AMFMcomp(𝐶₀)
+plot(𝐶₀; timeaxis=0.0:0.001:10.0)
+plot!( camera=(0,90),
+       zlabel="", zticks=:false,
+       left_margin=15Plots.mm, margin=5Plots.mm,
+       yrotation = 90, ymirror=true)
+png(path*"IS_exAMFM9.png")
