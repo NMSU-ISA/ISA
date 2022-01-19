7bca59bcbcb6b72b1038c75be9a2c76df8497a77
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2022-01-19 08:03:30 +0000

    build based on 4ff78ed

--- dev/assets/src/IS_num_exAMFMcomp.jl
+++
@@ -1,9 +0,0 @@
-path = "docs/src/assets/"
-
-using ISA, Plots
-ψ₀ = AMFMcomp(t->t,t->25cos(t),0.0)
-fs = 16_000
-t = 0:1/fs:1
-𝚿₀ = numComp( ψ₀(t), fs )
-plot(𝚿₀)
-png(path*"IS_num_exAMFMcomp1.png")
