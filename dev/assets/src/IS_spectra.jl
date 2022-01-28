5ff4039fdd0f3215dfdeaceff26bd042800c551c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2022-01-28 08:39:13 +0000

    build based on 20f2ba5

--- dev/assets/src/IS_spectra.jl
+++ dev/assets/src/IS_spectra.jl
@@ -23,5 +23,6 @@ using ISA, Plots
 𝐶₁ = AMFMtriplet(t->1.0,t->100*t,0.1)
 𝐶₂ = AMFMtriplet(t->0.8*cos(11t),t->100 + 70.5*sin(5t),π)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
-plot(𝑆,timeaxis=0.0:0.001:3.0,view="TF",left_margin=15Plots.mm, margin=5Plots.mm)
+plot(𝑆,timeaxis=0.0:0.001:3.0,view="TF",
+     left_margin=15Plots.mm, margin=5Plots.mm)
 png(path*"IS_ex2.png")
