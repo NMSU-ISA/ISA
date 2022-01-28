677a6373224b0a003ff43172337016a98f6d0142
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2022-01-28 07:30:38 +0000

    build based on 706d228

--- dev/assets/src/models.jl
+++ dev/assets/src/models.jl
@@ -24,11 +24,8 @@ using ISA, Plots
 𝐶₂ = AMFMtriplet(t->0.8*cos(11t),t->100 + 70.5*sin(5t),π)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 z = AMFMmodel(𝑆)
-plot(𝑆; timeaxis=-1.0:0.001:1.0)
-plot!( camera=(0,90),
-       zlabel="", zticks=:false,
-       left_margin=15Plots.mm, margin=5Plots.mm,
-       yrotation = 90, ymirror=true)
+plot(𝑆, timeaxis=-1.0:0.001:1.0, view="TF",
+     left_margin=15Plots.mm, margin=5Plots.mm)
 png(path*"IS_exAMFMmodel3.png")
 
 #--------------------------------
@@ -56,42 +53,36 @@ using ISA, Plots
 𝐶₂ = AMFMtriplet(t->sin(11t),t->250,0.0)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 z = AMFMmodel(𝑆)
-plot(𝑆; timeaxis=-1.0:0.001:1.0)
-plot!( camera=(0,90),
-       zlabel="", zticks=:false,
-       left_margin=15Plots.mm, margin=5Plots.mm,
-       yrotation = 90, ymirror=true)
+plot(𝑆, timeaxis=-1.0:0.001:1.0, view="TF",
+     left_margin=15Plots.mm, margin=5Plots.mm)
 png(path*"IS_exAMFMmodel6.png")
 
 #---------------------------
 
 using ISA, Plots
-𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+1,1.0)
+𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+100,1.0)
 𝐶₁ = AMFMtriplet(t->5exp(0.1t),t->250,0.0)
-𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t),0.0)
+𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t)+50,0.0)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 z = AMFMmodel(𝑆)
 plot(z; timeaxis=-1.0:0.001:1.0)
 png(path*"IS_exAMFMmodel7.png")
 
 using ISA, Plots
-𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+1,1.0)
+𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+100,1.0)
 𝐶₁ = AMFMtriplet(t->5exp(0.1t),t->250,0.0)
-𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t),0.0)
+𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t)+50,0.0)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 z = AMFMmodel(𝑆)
 plot(𝑆; timeaxis=-1.0:0.001:1.0)
 png(path*"IS_exAMFMmodel8.png")
 
 using ISA, Plots
-𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+1,1.0)
+𝐶₀ = AMFMtriplet(t->10,t->10t^2+2t+100,1.0)
 𝐶₁ = AMFMtriplet(t->5exp(0.1t),t->250,0.0)
-𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t),0.0)
+𝐶₂ = AMFMtriplet(t->10t,t->5cos(50t)+50,0.0)
 𝑆 = compSet([𝐶₀,𝐶₁,𝐶₂])
 z = AMFMmodel(𝑆)
-plot(𝑆; timeaxis=-1.0:0.001:1.0)
-plot!( camera=(0,90),
-       zlabel="", zticks=:false,
-       left_margin=15Plots.mm, margin=5Plots.mm,
-       yrotation = 90, ymirror=true)
+plot(𝑆, timeaxis=-1.0:0.001:1.0, view="TF",
+     left_margin=15Plots.mm, margin=5Plots.mm)
 png(path*"IS_exAMFMmodel9.png")
