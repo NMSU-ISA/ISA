872c6c0a5cc9c3951a0a7da2bddc688b9c620594
Steven Sandoval <ssandova@gmail.com>
2022-02-09 10:37:48 -0700

    special Functions

--- src/ISA.jl
+++ src/ISA.jl
@@ -3,20 +3,23 @@ module ISA
 include("components/analyticComponents.jl")
 export AMFMtriplet, AMFMcomp
 
+include("components/numericalComponents.jl")
+export numComp, numTriplet
+
+include("demod/demodulation.jl")
+export derivApprox, AMFMdemod
+
 include("models/analyticModels.jl")
 export compSet, AMFMmodel
 
 include("models/fourierSeries.jl")
 export fourierSeries
 
-include("components/numericalComponents.jl")
-export latentSig, latentComp, numComp, numTriplet
-
 include("models/numericalModels.jl")
 export numSet, numModel
 
-include("demod/demodulation.jl")
-export derivApprox, AMFMdemod
+include("other/specialFunctions.jl")
+export u, 𝒩ᵤ, 𝒩, δ, δn
 
 include("plots/GRrecipes.jl")
 
