8a60cdf24afd4f6c7ceb7bb174b55f3d7092d2fe
Steven Sandoval <ssandova@gmail.com>
2022-02-09 18:59:52 -0700

    logo

--- docs/src/assets/src/logo.jl
+++ docs/src/assets/src/logo.jl
@@ -2,18 +2,20 @@ path = "docs/src/assets/"
 
 using ISA, Plots
 fs = 16_000
-a₀(t) = exp(-1/2*(t-0.5)^2/(1.0/(10.0))^2)
-ω₀(t) = 2π*(70 + 50*sin(2π*2*t))
-𝐶₀ = AMFMtriplet(a₀,ω₀)
-a₁(t) = 0.5 + 1/3*sin(20.0*t)
-ω₁(t) = 2π*100.0
-𝐶₁ = AMFMtriplet(a₁,ω₁)
-a₂(t) = 0.54 - 0.46 * cos.(2π*t)
-ω₂(t) = 2π*(10.0 + exp(t+4.5)-exp(4.5))
-𝐶₂ = AMFMtriplet(a₂,ω₂)
-plot(𝐶₀,timeaxis=0.0:1/fs:1.0)
-plot!(𝐶₁,timeaxis=0.0:1/fs:1.0)
-plot!(𝐶₂, timeaxis=0.0:1/fs:1.0, camera = (7.5,80),
-xticks=:false,yticks=:false ,zticks=:false,
-xlabel = "",ylabel = "",zlabel = "")
+t = 0.0:1/fs:3.0
+𝐶₀ = AMFMtriplet(t->0.2+0.8cos(11t), t->200.0, 0.0)
+𝐶₁ = AMFMtriplet(t->exp(-abs(t/3)), t->100t, 0.1)
+𝐶₂ = AMFMtriplet(t->𝒩ᵤ(t; μ=1.5, σ=1.0), t->150 + 125sin(5t), π)
+𝐶₃ = AMFMtriplet(t->u(t-1.725)-u(t-2.475), t->50, 0.0)
+plot(𝐶₀,timeaxis=t)
+plot!(𝐶₁,timeaxis=t)
+plot!(𝐶₂,timeaxis=t)
+plot!(𝐶₃,linealpha = sign.(abs.(𝐶₃.a.(t))),timeaxis = t)
+plot!(camera = (7.5, 80),
+xticks = :false,
+yticks = :false,
+zticks = :false,
+xlabel = "",
+ylabel = "",
+zlabel = "",)
 png(path*"logo.png")
