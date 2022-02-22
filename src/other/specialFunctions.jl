b7cad7cff51e401fe3fb78c7095e649a8696cbea
Steven Sandoval <ssandova@gmail.com>
2022-02-22 14:00:02 -0700

    docs

--- src/other/specialFunctions.jl
+++ src/other/specialFunctions.jl
@@ -1,13 +1,13 @@
 
-@doc raw"""
+"""
     u(t::Float64)::Float64
 
 Unit Step Function
 
-``\mathrm{u}(t) = \begin{cases}
+$\mathrm{u}(t) = \begin{cases}
         1, &   t \geq 0 \\
         0, &   t < 0
-        \end{cases}``
+        \end{cases}$
 
 # Examples
 ```@example
